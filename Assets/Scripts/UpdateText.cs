32c6d53d68093fa6ede081a2fcdb4d19fa40cbc3
JuanCarlosGI <juancarlos.gi94@gmail.com>
2017-11-29 16:40:40 -0600

    Sync time and target texts

---
+++ Assets/Scripts/UpdateText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class UpdateText : NetworkBehaviour {
+    public Text Text;
+    [SyncVar(hook="ChangeText")]
+    private string _string;
+
+    public void SetText(string text)
+    {
+        if (!isServer) return;
+        _string = text;
+    }
+
+    private void ChangeText(string text)
+    {
+        Text.text = text;
+    }
+}
